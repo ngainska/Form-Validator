32d996700816753ca0e6abedc4b5462edba601b2
ngainska <87204580+ngainska@users.noreply.github.com>
2021-09-23 16:13:54 +0100

    added checkLength

--- script.js
+++ script.js
@@ -37,6 +37,17 @@ if(input.value.trim() === ""){
 });
 }
 
+//check input length
+function checkLength(input, min, max){
+    if (input.value.length < min){
+showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+    } else if(input.value.length > max) {showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+    } else {
+        showSuccess(input);
+    }
+}
+
+
 //get field name
 function getFieldName(input){
 return input.id.charAt(0).toUpperCase() + input.id.slice(1);
@@ -46,4 +57,7 @@ return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 form.addEventListener("submit", function(e){
     e.preventDefault();
     checkRequired([username, email, password, password2]);
+checkLength(username, 3, 15);
+checkLength(password, 6, 25);
+
 });
